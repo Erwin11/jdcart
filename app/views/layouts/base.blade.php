d3abbee87f9c56f16c0c12c0cb65df93399fdf85
wisteriaflash <wisteriaflash@163.com>
2014-11-18 14:08:22 +0800

    修改favicon.ico的图片，及引入页面方式

--- app/views/layouts/base.blade.php
+++ app/views/layouts/base.blade.php
@@ -12,7 +12,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   {{-- token --}}
   <meta name="_token" content="{{ csrf_token() }}" />
-  <link rel="shortcut icon" href="{{ asset('assets/img/icon.png') }}">
   @section('beforeStyle')
     @show{{-- 页面内联样式之前 --}}
   <style>
